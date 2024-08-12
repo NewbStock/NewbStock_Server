1093d149a20fcd3cc4469747f9d145bfd11acee3
이승준 <127376832+hhee4455@users.noreply.github.com>
2024-08-12 13:04:20 +0900

    Test : 깃액션

--- NS_project/settings.py
+++ NS_project/settings.py
@@ -24,8 +24,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'drf_yasg',
-    'rest_framework',
     'NS_app',
+    'rest_framework',
+
 ]
 
 MIDDLEWARE = [
