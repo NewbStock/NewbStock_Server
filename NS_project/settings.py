bae4441b6d1060b1ebf0e6c155a61157f7db7964
Ubuntu <ubuntu@ip-10-0-138-141.ap-northeast-2.compute.internal>
2024-08-17 15:42:10 +0000

    로컬 변경 사항 커밋

--- NS_project/settings.py
+++ NS_project/settings.py
@@ -1,20 +1,23 @@
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# .env 파일에서 환경 변수 로드
+load_dotenv()
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
+DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j28@_g5v8zrscw9jra)dgb=-ajk4wrl^ikk_5joh4$72&91ph6'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-# Application definition
+ALLOWED_HOSTS = [
+    '10.0.138.141',
+    'team-won-2-alb-1672477310.ap-northeast-2.elb.amazonaws.com',
+    '127.0.0.1',
+    'localhost',
+    '54.180.65.81'
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -45,7 +48,7 @@ ROOT_URLCONF = 'NS_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -60,37 +63,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'NS_project.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',  # PostgreSQL 사용
-        'NAME': 'production',  # 실제 데이터베이스 이름
-        'USER': 'newbstock_admin',  # 실제 데이터베이스 사용자
-        'PASSWORD': 'Newbstock001!',  # 사용자 비밀번호
-        'HOST': 'localhost',  # RDS 엔드포인트
-        'PORT': '5432',  # PostgreSQL 기본 포트
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
-
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'production',
-#         'USER': 'newbstock_admin',
-#         'PASSWORD': 'Newbstock001!',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-
-
-
-# Password validation
-# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -107,10 +89,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/5.0/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -119,23 +97,9 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-ALLOWED_HOSTS = [
-	'10.0.138.141',
-	'team-won-2-alb-1672477310.ap-northeast-2.elb.amazonaws.com',
-    '127.0.0.1',
-    'localhost',
-	]
 
 LOGIN_REDIRECT_URL = '/'
