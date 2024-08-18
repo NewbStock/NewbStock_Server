dbdcd1a88e99b9a9492458705ec69fca2b657201
이승준 <127376832+hhee4455@users.noreply.github.com>
2024-08-18 21:26:36 +0900

    Fix : urls 수정

--- newb/urls.py
+++ newb/urls.py
@@ -7,12 +7,12 @@ router = DefaultRouter()
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('stocktopkr/', StockTop3View.as_view(), name='StockTop3View'),
-    path('highchange/', HighChangeStock.as_view(), name='HighChangeStock'),
-    path('charts/', TemplateView.as_view(template_name='charts.html'), name='charts'),
-    path('exchange/',ExchangeRateChartView.as_view(), name='ExchangeRateChartView'),
-    path('kospi/',KospiKosdaqChartView.as_view(), name='KospiChartView'),
-    path('interestrates/',InterestRateChartView.as_view(), name='InterestRatesChartView'),
-    path('newssentiment/',NewsSentimentChartView.as_view(), name='NewsSentimentChartView'),
+    path('stocktopkr', StockTop3View.as_view(), name='StockTop3View'),
+    path('highchange', HighChangeStock.as_view(), name='HighChangeStock'),
+    path('charts', TemplateView.as_view(template_name='charts.html'), name='charts'),
+    path('exchange',ExchangeRateChartView.as_view(), name='ExchangeRateChartView'),
+    path('kospi',KospiKosdaqChartView.as_view(), name='KospiChartView'),
+    path('interestrates',InterestRateChartView.as_view(), name='InterestRatesChartView'),
+    path('newssentiment',NewsSentimentChartView.as_view(), name='NewsSentimentChartView'),
 ]
 
